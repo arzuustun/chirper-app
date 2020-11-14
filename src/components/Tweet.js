395ada4779e9cb684f5bb82a44c43201ac31afa0
arzuustun <ustunarzu06@gmail.com>
2020-11-14 03:56:52 +0300

    using react router

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,10 +1,10 @@
-//const { Component } = require("react")
-
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import{formatTweet, formatDate} from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
 import{handleToggleTweet} from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
+
 class Tweet extends Component{
     handleLike = (e) => {
         e.preventDefault()
@@ -18,7 +18,7 @@ class Tweet extends Component{
       }
       toParent = (e, id) => {
         e.preventDefault()
-        // todo: Redirect to parent Tweet.
+        this.props.history.push(`/tweet/${id}`) // Redirect to parent Tweet.
       }
 
     render(){
@@ -30,11 +30,11 @@ class Tweet extends Component{
        // console.log(this.props)
 
         const {
-                name, avatar, timestamp, text, hasLiked, likes, replies, parent
+                name, avatar, timestamp, text, hasLiked, likes, replies, id,parent
               } = tweet
 
               return (
-                <div className='tweet'>
+                <Link to={`/tweet/${id}`} className='tweet'>
                   <img
                     src={avatar}
                     alt={`Avatar of ${name}`}
@@ -62,7 +62,7 @@ class Tweet extends Component{
                       <span>{likes !== 0 && likes}</span>
                     </div>
                   </div>
-                </div>
+                </Link>
               )
     }
 }
@@ -78,4 +78,4 @@ function mapStateToProps({ autedUser, users, tweets },{ id }){
     }
 
 }
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
